2491b20aa68a8b98087f653d1107b1084e1dbb07
Tim Schindler <tim.schindler@gmail.com>
2020-04-25 23:02:35 +0200

    version bump to 0.10 with new m3u and xml declaration detection

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setuptools.setup(
     name='pysmoothstreams',
-    version='0.9',
+    version='0.10',
     url='https://github.com/aaearon/pysmoothstreams',
     license='MIT',
     author='Tim Schindler',
