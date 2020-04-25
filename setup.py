6e36b5e78850871c289dd43b417b421f0d374818
Tim Schindler <tim.schindler@gmail.com>
2020-04-25 23:15:55 +0200

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setuptools.setup(
     name='pysmoothstreams',
-    version='0.10',
+    version='0.11',
     url='https://github.com/aaearon/pysmoothstreams',
     license='MIT',
     author='Tim Schindler',
